d370b32459e439419bdfb6576c93a5c3d004511e
deepak <deepakdeveloper03@gmail.com>
2024-12-30 22:41:33 +0530

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import {HeroSection,AboutMeSection, MyWorkSection,SkillSection,ContactSection} from "$components";
+
+	const {data}=$props();
+
+	//$inspect(data);
+
+	let {workExperience, projects,skills} =data;
+
+</script>
+<HeroSection></HeroSection>
+<AboutMeSection {workExperience}></AboutMeSection>
+<MyWorkSection {projects} />
+<SkillSection {skills}/>
+<ContactSection/>
